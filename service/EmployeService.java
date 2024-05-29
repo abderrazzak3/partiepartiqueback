36aa32887afd5e1368e82bad1450712f9c3d169f
abderrazzak3 <132061925+abderrazzak3@users.noreply.github.com>
2024-05-29 22:50:01 +0100

    A

---
+++ service/EmployeService.java
@@ -0,0 +1,30 @@
+package service;
+
+import java.util.List;
+
+import dao.EmployeDao;
+import model.Employe;
+
+public class EmployeService {
+    EmployeDao employeDao;
+
+    public EmployeService() {
+        employeDao = new EmployeDao();
+    }
+
+    public List<Employe> getAllEmployes() {
+        return employeDao.getAllEmployes();
+    }
+
+    public Employe addEmploye(Employe employe) {
+        return employeDao.addEmploye(employe);
+    }
+
+    public void deleteEmploye(Employe employe) {
+        employeDao.deleteEmploye(employe);
+    }
+
+    public Employe updateEmploye(Employe employe) {
+        return employeDao.updateEmploye(employe);
+    }
+}
